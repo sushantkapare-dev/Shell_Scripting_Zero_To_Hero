712e95667f74bdb690972fde1152251c4dc6a62f
sushantkapare1717@gmail.com <sushant@13.com>
2023-08-11 11:27:37 +0530

    modify day 11

--- 11_Logical-ops.sh
+++ 11_Logical-ops.sh
@@ -2,7 +2,17 @@
 
 read -p "Enter your age :" age
 read -p "Enter your country: " country
-if [[ $age -ge 18 ]] && [[ $country == "india" ]]
+
+# AND Operator
+# if [[ $age -ge 18 ]] && [[ $country == "india" ]]
+# then
+#         echo "You can vote"
+# else
+#         echo "You cant vote "
+# fi
+
+OR Operator
+if [[ $age -ge 18 ]] || [[ $country == "india" ]]
 then
         echo "You can vote"
 else
