7523b0a1ae69307f4ed3bb1a24bc7191859c7a15
sushantkapare1717@gmail.com <sushant@13.com>
2023-08-18 10:07:53 +0530

    modify day 19

--- 19_arguments.sh
+++ 19_arguments.sh
@@ -5,3 +5,10 @@ echo "Second argument is $2"
 
 echo "All the argunets are- $@"
 echo "Number of arguments are: $#"
+
+# for loop to access files from arguments
+
+for filename in $@
+do
+        echo "copying files- $filename"
+done
